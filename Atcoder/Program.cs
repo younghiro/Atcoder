58a49d98d1de6930a7653b783a7f7f37ea5af910
younghiro <younghiromu112010@icloud.com>
2022-10-21 14:47:02 +0200

    2022/10/21

--- Atcoder/Program.cs
+++ Atcoder/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] c = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int output1 = c[0] | c[1];
+        int output2 = c[0] & c[1];
+        Console.WriteLine(output1);
+        Console.WriteLine(output2);
 
+    }
+}
